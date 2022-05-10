ae9420a3aa09882245cfb1ae22857b51a44e257a
Jamie Metcalfe <jamie.jet18@gmail.com>
2022-05-10 15:43:38 -0500

    Add files via upload
    Learning some basics stuff with a fizzbuzz!

---
+++ fizzbuzz1.js
@@ -0,0 +1,9 @@
+
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
