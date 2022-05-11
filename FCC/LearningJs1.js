ea85624ff9bec98633e3c93daf13176fa16c50e3
Jamie Metcalfe <jamie.jet18@gmail.com>
2022-05-11 00:12:07 -0500

    Add files via upload
    FCC given it's own folder for my exercises, projects, and notes.

---
+++ FCC/LearningJs1.js
@@ -0,0 +1,40 @@
+ // Basics:
+
+
+// Comments:
+// This is an in-line comment.
+
+/* This is a
+multi-line comment */
+
+// Declaring vars:
+var myName;
+
+//Storing Values with the Assignment Operator
+var a;
+a = 7;
+
+//assigning the value of one var to another
+var a;
+a = 7;
+var b;
+b = a;
+
+//initializing variables with the assignment operator
+var a = 9;
+
+//declaring string variables
+var myFirstName = "jamie";
+var myLastName = "metcalfe";
+
+//understanding unitialised variables
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + "String!";
+
+
